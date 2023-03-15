cd65e5411e8da2b5b6463d0accc91afe8d8948be
MuchinaWamburia <lucymuchina20@gmail.com>
2023-03-15 09:06:56 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,19 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student 
+  end
+  
+  def index
+    name_query = params[:name]
+    if name_query.blank?
+      students = Student.all
+    else
+      students = Student.where('first_name LIKE :query OR last_name LIKE :query', query: "%#{name_query}%")
+    end
+    render json: students
+  end
+
 end
